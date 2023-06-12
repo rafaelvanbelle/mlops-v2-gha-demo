b5405a54eca6deb292cefc0610d8a8756f321570
Rafael Van Belle <rafael@dataroots.io>
2023-06-12 15:19:32 +0200

    update to terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,18 @@
 terraform {
-  backend "azurerm" {} 
+  cloud {
+    organization = "tutorial-terraform-raf"
+
+    workspaces {
+      name = "terraform-github-actions"
+    }
+  }
   required_providers {
     azurerm = {
       version = "= 2.99.0"
     }
   }
 }
+
 
 provider "azurerm" {
   features {}
